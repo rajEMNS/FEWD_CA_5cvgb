1a232eda718f4624863da4b6176cbbb2d5488ab9
hanshul22 <141112472+hanshul22@users.noreply.github.com>
2023-12-20 10:59:28 +0530

    Update Forms.jsx

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -4,30 +4,42 @@ import "../App.css";
 import logo from "../assets/like.png";
 
 function Form() {
+  // Initial form values
   const initValues = { name: "", email: "", password: "", repeat: "" };
+  // State to manage form data
   const [Fdata, setFdata] = useState(initValues);
+  // State to manage registration success
   const [register, setRegister] = useState(false);
+  // State to manage form validation errors
   const [errs, seterrs] = useState({});
 
+  // Function to handle form input changes
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFdata({ ...Fdata, [name]: value });
   };
 
+  // Function to handle form submission
   const Submit = (e) => {
     e.preventDefault();
+    // Validate form data
     const validationErrors = validation(Fdata);
+    // Set validation errors
     seterrs(validationErrors);
+    // If there are no validation errors, set registration success to true
     if (Object.keys(validationErrors).length === 0) {
       setRegister(true);
     }
   };
 
+  // Effect to perform actions after successful registration
   useEffect(() => {
     if (Object.keys(errs).length === 0 && register) {
+      // Perform actions after successful registration if needed
     }
   }, [errs, register]);
 
+  // Function to validate form data
   const validation = (values) => {
     const err = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
@@ -64,6 +76,7 @@ function Form() {
 
   return (
     <>
+      {/* Display success message if there are no errors and registration is successful */}
       {Object.keys(errs).length === 0 && register ? (
         <div
           className="success"
@@ -73,10 +86,12 @@ function Form() {
           <img src={logo} alt="" />
         </div>
       ) : null}
+      {/* Registration form */}
       <div className="forms">
         <div>
           <form onSubmit={Submit}>
             <h1 className="heading">Registration Form</h1>
+            {/* Input for Name */}
             <div className="boxe">
               <label className="input-titles">Name:</label>
               <input
@@ -88,7 +103,9 @@ function Form() {
                 placeholder="  Enter your name"
               />
             </div>
+            {/* Display error for Name */}
             <div className="error">{errs.name}</div>
+            {/* Input for Email */}
             <div className="boxe">
               <label className="input-titles">Email:</label>
               <input
@@ -100,7 +117,9 @@ function Form() {
                 placeholder="  email@gmail.com"
               />
             </div>
+            {/* Display error for Email */}
             <div className="error">{errs.email}</div>
+            {/* Input for Password */}
             <div className="boxe">
               <label className="input-titles">Password:</label>
               <input
@@ -112,7 +131,9 @@ function Form() {
                 placeholder="  **********"
               />
             </div>
+            {/* Display error for Password */}
             <div className="error">{errs.password}</div>
+            {/* Input for Confirm Password */}
             <div className="boxe">
               <label className="input-titles">Confirm Password:</label>
               <input
@@ -125,7 +146,9 @@ function Form() {
                 placeholder="  **********"
               />
             </div>
+            {/* Display error for Confirm Password */}
             <div className="error">{errs.repeat}</div>
+            {/* Sign-in link and submit button */}
             <div className="signin">
               <p className="text">
                 Already have an account?
